c1f181c6363e83d9ef2b2526f01b5ffff040706f
Natalia Moskovka <nat.chan.des@yandex.ru>
2022-03-19 14:40:51 +0300

    Добавлен пустой проект с заданной в нем структурой

---
+++ app/src/main/java/ru/kubgu/techsupport/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.kubgu.techsupport.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.kubgu.techsupport.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
